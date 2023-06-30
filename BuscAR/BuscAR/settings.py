626aebcb0d3ebe85ad2bf60728149f6941d9fdc5
albonardo <leonel_albino@hotmail.com>
2023-06-30 18:53:17 -0300

    Creacion de nueva clase Account y accesorias para modificación de comportamientos acorde al diseño planteado.

--- BuscAR/BuscAR/settings.py
+++ BuscAR/BuscAR/settings.py
@@ -27,6 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+AUTH_USER_MODEL = "app_mvc.Account"
 
 # Application definition
 
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app_mvc',
 ]
 
 MIDDLEWARE = [
@@ -103,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-es'
 
 TIME_ZONE = 'UTC'
 
--- BuscAR/app_mvc/admin.py
+++ BuscAR/app_mvc/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from app_mvc.models import Account
 
-# Register your models here.
+
+class AccountAdmin(UserAdmin):
+    list_display = ('email', 'username', 'date_joined', 'last_login', 'is_active')
+    search_fields = ('email', 'username')
+    readonly_fields = ('id', 'date_joined', 'last_password_change', 'is_admin', 'is_staff', 'is_superuser')
+
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = ()
+
+
+admin.site.register(Account, AccountAdmin)
