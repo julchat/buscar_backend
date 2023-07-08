f96848585c2e26c6033d700b449a035bad4b51e8
albonardo <leonel_albino@hotmail.com>
2023-07-08 20:57:42 -0300

    update settings.py

--- BuscAR/BuscAR/settings.py
+++ BuscAR/BuscAR/settings.py
@@ -29,7 +29,7 @@ if DEBUG:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend' # DEVelopment only
 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0', '192.168.0.128']
 
 AUTH_USER_MODEL = "app_mvc.Account"
 
